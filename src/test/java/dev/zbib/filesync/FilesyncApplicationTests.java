72986cdf2d1a003db21c385c4edc65ab7ece1199
Mohammad Zbib <mohammad.h.zbib@gmail.com>
2026-05-28 01:30:18 +0300

    init

---
+++ src/test/java/dev/zbib/filesync/FilesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zbib.filesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
