77018ffd92973032c7120bf077215faf3c8de533
Mohammad Zbib <mohammad.h.zbib@gmail.com>
2026-05-31 01:44:51 +0300

    chore(storage): remove not needed minio code

--- src/main/java/dev/zbib/drive/infrastructure/minio/ExposedMinioClient.java
+++
@@ -1,9 +0,0 @@
-package dev.zbib.drive.infrastructure.minio;
-
-import io.minio.MinioClient;
-
-public class ExposedMinioClient extends MinioClient {
-    protected ExposedMinioClient(MinioClient client) {
-        super(client);
-    }
-}
